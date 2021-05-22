96848b863401f05b7187b48bf1224b567b88e7f1
Jonatasasr <Jonatasasr@users.noreply.github.com>
2021-05-21 21:19:39 -0300

    Update Cadastro
    Finish

--- HTML/Frontend/php/cadastro.php
+++ HTML/Frontend/php/cadastro.php
@@ -9,16 +9,12 @@ $tel = $_POST ["telefone"];
 $cep = $_POST ["cep"]; 
 $senha = $_POST ["senha"]; 
 $confsenha = $_POST["confsenha"];
+ 
 
-
-
-//mysqli_query($conexao,$query)
-//mysqli_close($conexao); 
-
-$query_select = "SELECT email FROM cliente WHERE email = '$email'";
+$query_select = "SELECT telefone FROM cliente WHERE telefone = '$tel'";
 $select = mysqli_query($conexao,$query_select);
 $array = mysqli_fetch_array($select);
-$logarray = $array['email'];
+$logarray = $array['telefone'];
 
 
 
@@ -29,10 +25,10 @@ if($senha != $confsenha){
 }else{
 
 
-if($logarray == $email){
+if($logarray == $tel){
 
     echo"<script language='javascript' type='text/javascript'>
-    alert('Esse email já existe');window.location.href='/mercado-paid-egua/HTML/frontend/sign_up.html';</script>";
+    alert('Esse telefone já existe');window.location.href='/mercado-paid-egua/HTML/frontend/sign_up.html';</script>";
     die();
 
 }else{
@@ -45,10 +41,12 @@ if($logarray == $email){
       echo"<script language='javascript' type='text/javascript'>
       alert('Usuário cadastrado com sucesso!');window.location.
       href='/mercado-paid-egua/HTML/frontend/sign_in.html'</script>";
+      die();
     }else{
       echo"<script language='javascript' type='text/javascript'>
       alert('Não foi possível cadastrar esse usuário');window.location
       .href='/mercado-paid-egua/HTML/frontend/sign_up.html'</script>";
+      die();
     }
   }
 }
