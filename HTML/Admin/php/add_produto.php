8634e06b5b1156fae35121896fd2cf25106c5c5c
Carlos Daniel <71909870+c-daniel-alves@users.noreply.github.com>
2021-06-02 10:32:47 -0300

    add_produto
    Adicionar nome e valor do produto add

--- HTML/Admin/php/add_produto.php
+++ HTML/Admin/php/add_produto.php
@@ -4,17 +4,18 @@
 
         $pro_nome = $_POST['p_nome'];
         $pro_desc =  $_POST['p_desc'];
-        $pro_cat = $_POST['p_cat'];
+        //$pro_cat = $_POST['p_cat'];
         $pro_valor = $_POST['p_valor'];
-        $pro_desconto = $_POST['p_desconto'];
-        $pro_ativo = $_POST['status'];
+        //$pro_desconto = $_POST['p_desconto'];
+        //$pro_ativo = $_POST['status'];
 
-        $SQL = "INSERT INTO produtos (pro_nome, pro_desc, pro_cat, pro_valor, pro_desconto, pro_ativo)
-                VALUES ('$pro_nome','$pro_desc','$pro_cat','$pro_valor', '$pro_desconto', '$pro_ativo') ";
+        $query = "INSERT INTO produtos (pro_nome, pro_desc, pro_valor)
+                VALUES ('$pro_nome', '$pro_desc', '$pro_valor')";
 
-        $db->query($SQL) or die("Query Falhou");
+        $insert = mysqli_query($conexao,$query);
 
-        header("Location: Admin/products.html")
+       // $db->query($SQL) or die("Query Falhou");
+
 
 
 
