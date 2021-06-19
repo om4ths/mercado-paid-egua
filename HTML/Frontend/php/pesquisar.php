6c2fe97aa0e46e84565e1274df299f2973046781
Jonatasasr <Jonatasasr@users.noreply.github.com>
2021-06-19 15:10:31 -0300

    Pesquisar

--- HTML/Frontend/php/pesquisar.php
+++ HTML/Frontend/php/pesquisar.php
@@ -1,24 +1,35 @@
 <?php
-$userbd = ('localhost');
-$usuario = ('root');
-$senha = ('');
-$bd = ('mercado');
-$conexao = mysqli_connect($userbd,$usuario,$senha,$bd) or die ('Não foi possível conectar');
 
+    include("conexao.php");
 
-include("conexao.php");
-
-    $pesquisar = $_POST['pesquisar'];
-    $result_produtos = "SELECT * FROM produtos WHERE pro_nome LIKE '%$pesquisar%' LIMIT 5";
-
-    $resultado_produtos = mysqli_query($conn, $result_produtos);
-
-    while($rows_produtos = mysqli_fetch_array($resultado_produtos)){
-        echo "Nome do curso: ".$rows_produtos['pro_nome']."<br>";
-
+    $pesquisar = $_GET['pesquisar'];
+    $result_seach = "SELECT * FROM produtos WHERE pro_nome LIKE '%$pesquisar%' LIMIT 5";
+    $array_seach = mysqli_query($conexao, $result_seach);
+    while($r_seach_prow = mysqli_fetch_array($array_seach)){
+        $r_seach_pro[] = $r_seach_prow;
     }
+    ?>
+    <?php 
+	if(isset($r_seach_pro))
+	{	
+	 foreach($r_seach_pro as $key => $r_seach)
+	{
+    ?>
+       <p><?php echo $r_seach['pro_nome']; ?> </p>
+       <p><?php echo $r_seach['pro_cat']; ?> </p>
+       <p><?php echo $r_seach['pro_id']; ?> </p>
 
 
+    																		
+	    
+
+
+
+
+    <?php
+		};
+	};
+	 
 
 
 ?>
