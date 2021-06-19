b8ca36c05003275ef23f0d43ffc5f24b158d6585
Jonatasasr <Jonatasasr@users.noreply.github.com>
2021-06-18 23:32:07 -0300

    Dani

--- HTML/Admin/php/ver_produto.php
+++ HTML/Admin/php/ver_produto.php
@@ -7,10 +7,10 @@
 
 
         $view_pro_cat = $r_view_pro['pro_cat'];
-        $select_pro_cat = "SELECT * FROM produtos WHERE pro_cat = '$view_pro_cat' order by pro_id desc LIMIT 0, 3 ";   
+        $select_pro_cat = "SELECT * FROM produtos WHERE pro_cat = '$view_pro_cat' order by pro_data_criado desc LIMIT 0, 3 ";   
 		$array_pro_cat = mysqli_query($conexao,$select_pro_cat);
 		while($r_prow_cat = mysqli_fetch_array($array_pro_cat)){
-			$r_produtos_cat[] = $r_prow_cat; 
+			$r_produtos_cat[] = $r_prow_cat;
 		}
 
 ?>
