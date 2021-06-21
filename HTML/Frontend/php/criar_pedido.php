aa3aa8bb1da3270f218fe1b1a47aa9acca1d8a05
Jonatasasr <Jonatasasr@users.noreply.github.com>
2021-06-21 09:38:25 -0300

    update check 0.9

--- HTML/Frontend/php/criar_pedido.php
+++ HTML/Frontend/php/criar_pedido.php
@@ -3,9 +3,11 @@ include('func.php');
 include("carrinho.php");
 
 $id_cliente = $_SESSION['telefone'];
+$id_pedido = random_int(1, 9999) ;
 $id_end = $_GET['id_end'];
-echo "id_usuario: <br>".$id_cliente;
-echo "<br>id_end: <br>".$id_end;
+//echo "id_usuario: <br>".$id_cliente;
+//echo "<br>id_endereco: <br>".$id_end;
+//echo "<br>id_pedido: <br>".$id_pedido;
 
 $valor_total = number_format($totalCfrete, 2, ',', '.');
 $frete = $frete ;
@@ -14,17 +16,28 @@ $total_prod = number_format($totalCarts, 2, ',', '.');
 
 
 
-echo "<br><br><br>Valor total: <br>".$valor_total;
-echo "<br>Valor do Frete: <br>".$frete;
-echo "<br>Valor Total do Desconto: <br>".$valor_total_desc;
-echo "<br>Total de Produtos: <br>".$total_prod;
+//echo "<br><br><br>Valor total: <br>".$valor_total;
+//echo "<br>Valor do Frete: <br>".$frete;
+//echo "<br>Valor Total do Desconto: <br>".$valor_total_desc;
+//echo "<br>Total de Produtos: <br>".$total_prod;
 
 if($resultsCarts){
 foreach($resultsCarts as $result) : 
-echo "<br><br><br>Dentro do for ID: <br>".$result['id'];
-echo "<br>Dentro do for Nome: <br>".$result['name'];
-echo "<br>Dentro do for Valor com desconto: <br>".$result['discount'];
-echo "<br>Dentro do for Valor sem Desconto <br>".$result['price'];
+
+echo "id_usuario: <br>".$id_cliente;
+echo "<br>id_endereco: <br>".$id_end;
+echo "<br>id_pedido: <br>".$id_pedido;
+echo "<br>Valor total: <br>".$valor_total;
+echo "<br>Valor do Frete: <br>".$frete;
+echo "<br>Valor Total do Desconto: <br>".$valor_total_desc;
+echo "<br>Total de Produtos: <br>".$total_prod;
+echo "<br>id_pedido: <br>".$id_pedido;
+echo "<br>Produto ID: <br>".$result['id'];
+echo "<br>Produto Nome: <br>".$result['name'];
+echo "<br>Quantidade: <br>".$result['quantity'];
+echo "<br>Valor com desconto: <br>".$result['discount'];
+echo "<br>Valor sem Desconto <br>".$result['price'];
+echo "<br><br><br>";
 endforeach;
 }
 
