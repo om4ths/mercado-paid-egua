0207fb7dd7469bd9d4e8ba2aeeb758f10d87341a
Jonatasasr <Jonatasasr@users.noreply.github.com>
2021-06-21 11:49:41 -0300

    Update exibe_pedidos.php

--- HTML/Frontend/php/exibe_pedidos.php
+++ HTML/Frontend/php/exibe_pedidos.php
@@ -8,5 +8,12 @@
 		$r_view_pedido = mysqli_fetch_array($array_view_pedido);
 
 
+		$id_order = $r_view_pedido['pedido_id'];
+
+		$select_view_end = "SELECT * FROM `endereco` WHERE id_telefone = '$view_telefone' AND id='$id_order'";   
+		$array_view_end = mysqli_query($conexao,$select_view_end);
+		$r_view_end = mysqli_fetch_array($array_view_end);
+
+
 
 ?>
