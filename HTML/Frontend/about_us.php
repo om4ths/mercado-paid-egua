140d1aa225b31e5cabc4c235227e0a5d476c63cf
Carlos Daniel <71909870+c-daniel-alves@users.noreply.github.com>
2021-06-21 01:12:31 -0300

    fsdfsd

--- HTML/Frontend/about_us.php
+++ HTML/Frontend/about_us.php
@@ -79,6 +79,7 @@ include('php/dados_cliente.php');
 		</div>
 	</div>
 	<!-- Categoria fim-->
+	
 	<!-- pesquisar produtos inicio-->
 	<div id="search_model" class="header-cate-model main-gambo-model modal fade" tabindex="-1" role="dialog" aria-modal="false">
 		<div class="modal-dialog search-ground-area" role="document">
@@ -87,72 +88,6 @@ include('php/dados_cliente.php');
 					<button type="button" class="close btn-close" data-dismiss="modal" aria-label="Close">
 						<i class="uil uil-multiply"></i>
 					</button>
-				</div>
-				<div class="category-model-content modal-content">
-					<div class="search-header">
-						<form action="#">
-							<input type="search" placeholder="Search for products...">
-							<button type="submit"><i class="uil uil-search"></i></button>
-						</form>
-					</div>
-					<div class="search-by-cat">
-						<a href="#" class="single-cat">
-							<div class="icon">
-								<img src="images/category/icon-1.svg" alt="">
-							</div>
-							<div class="text">
-								Frutas e Vegetais
-							</div>
-						</a>
-						<a href="#" class="single-cat">
-							<div class="icon">
-								<img src="images/category/icon-2.svg" alt="">
-							</div>
-							<div class="text"> Alimentos Básicos </div>
-						</a>
-						<a href="#" class="single-cat">
-							<div class="icon">
-								<img src="images/category/icon-3.svg" alt="">
-							</div>
-							<div class="text"> Ovos e Laticínios </div>
-						</a>
-						<a href="#" class="single-cat">
-							<div class="icon">
-								<img src="images/category/icon-4.svg" alt="">
-							</div>
-							<div class="text"> Bebidas </div>
-						</a>
-						<a href="#" class="single-cat">
-							<div class="icon">
-								<img src="images/category/icon-5.svg" alt="">
-							</div>
-							<div class="text"> Lanches </div>
-						</a>
-						<a href="#" class="single-cat">
-							<div class="icon">
-								<img src="images/category/icon-6.svg" alt="">
-							</div>
-							<div class="text"> Limpeza </div>
-						</a>
-						<a href="#" class="single-cat">
-							<div class="icon">
-								<img src="images/category/icon-7.svg" alt="">
-							</div>
-							<div class="text"> Macarrão e molhos </div>
-						</a>
-						<a href="#" class="single-cat">
-							<div class="icon">
-								<img src="images/category/icon-8.svg" alt="">
-							</div>
-							<div class="text"> Higiene </div>
-						</a>
-						<a href="#" class="single-cat">
-							<div class="icon">
-								<img src="images/category/icon-9.svg" alt="">
-							</div>
-							<div class="text"> Pets </div>
-						</a>
-					</div>
 				</div>
 			</div>
 		</div>
