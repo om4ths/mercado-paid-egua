3bdd974ebaf94b407cd20b910a7eb14424917f2b
Jonatasasr <Jonatasasr@users.noreply.github.com>
2021-06-21 01:11:17 -0300

    update about

--- HTML/Frontend/about_us.php
+++ HTML/Frontend/about_us.php
@@ -372,7 +372,12 @@ include('php/dados_cliente.php');
 					<a href="#" class="cate__btn" data-toggle="modal" data-target="#category_model" title="Categories"><i class="uil uil-apps"></i></a>
 				</div>
 				<div class="header_cart order-1">
-					<a href="#" class="cart__btn hover-btn pull-bs-canvas-left" title="Cart"><i class="uil uil-shopping-cart-alt"></i><span>Carrinho</span><ins>2</ins><i class="uil uil-angle-down"></i></a>
+				<!-- MOSTRAR A QUANTIDADE NO CARRINHO -->
+				<?php if($cont_itens >= 1) { ?>
+						<a href="#" class="cart__btn hover-btn pull-bs-canvas-left" title="Cart"><i class="uil uil-shopping-cart-alt"></i><span>Carrinho</span><ins><?php echo $cont_itens ?></ins><i class="uil uil-angle-down"></i></a>
+					<?php }else{ ?>
+						<a href="#" class="cart__btn hover-btn pull-bs-canvas-left" title="Cart"><i class="uil uil-shopping-cart-alt"></i><span>Carrinho</span><ins>0</ins><i class="uil uil-angle-down"></i></a>
+					<?php } ?>
 				</div>
 				<div class="search__icon order-1">
 					<a href="#" class="search__btn hover-btn" data-toggle="modal" data-target="#search_model" title="Search"><i class="uil uil-search"></i></a>
