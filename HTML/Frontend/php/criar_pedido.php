aec016847d1752f486b176d8fa7be97d36fac56d
Jonatasasr <Jonatasasr@users.noreply.github.com>
2021-06-21 10:02:36 -0300

    verificar endereço check

--- HTML/Frontend/php/criar_pedido.php
+++ HTML/Frontend/php/criar_pedido.php
@@ -4,22 +4,16 @@ include("carrinho.php");
 
 $id_cliente = $_SESSION['telefone'];
 $id_pedido = random_int(1, 9999) ;
+
+
+if($_GET['id_end']){
 $id_end = $_GET['id_end'];
-//echo "id_usuario: <br>".$id_cliente;
-//echo "<br>id_endereco: <br>".$id_end;
-//echo "<br>id_pedido: <br>".$id_pedido;
 
 $valor_total = number_format($totalCfrete, 2, ',', '.');
 $frete = $frete ;
 $valor_total_desc = number_format($totaldesc, 2, ',', '.');
 $total_prod = number_format($totalCarts, 2, ',', '.');
 
-
-
-//echo "<br><br><br>Valor total: <br>".$valor_total;
-//echo "<br>Valor do Frete: <br>".$frete;
-//echo "<br>Valor Total do Desconto: <br>".$valor_total_desc;
-//echo "<br>Total de Produtos: <br>".$total_prod;
 
 if($resultsCarts){
 foreach($resultsCarts as $result) : 
@@ -40,6 +34,10 @@ echo "<br>Valor sem Desconto <br>".$result['price'];
 echo "<br><br><br>";
 endforeach;
 }
-
+}else{
+    $_SESSION['msg_check'] = "<script language='javascript' type='text/javascript'>
+    alert('Endereço não encontrado');</script>";
+      header("Location: /mercado-paid-egua/HTML/frontend/checkout.php");
+}
 
 ?>
